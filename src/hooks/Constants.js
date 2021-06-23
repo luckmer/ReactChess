7acc2b3070a2bf1f83607e537f47f4c582f0f877
luckmer <woszczo04@gmail.com>
2021-06-23 15:02:07 +0200

    data to display letters / numbers

---
+++ src/hooks/Constants.js
@@ -0,0 +1,6 @@
+export const letters = ["a", "b", "c", "d", "e", "f", "g", "h"];
+export const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+export const GRID_SIZE = 8;
+export const TOTAL_SIZE = 8 ** 2 - 1;
+export const MAX = 8 ** 2;
+export const direction = -1;
