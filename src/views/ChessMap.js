56ba9855a9d55a54b3a7a6342a90795455623836
luckmer <woszczo04@gmail.com>
2021-06-23 15:03:32 +0200

    board creator

--- src/views/ChessMap.js
+++ src/views/ChessMap.js
@@ -1,5 +1,33 @@
+import { BoardStyle, Div } from "../css/Chess.Style";
+import ChessCreator from "../hooks/Board/ChessGrid";
+import DragAndDrop from "../hooks/DragAndDrop/DragAndDrop";
+
 const ChessMap = () => {
-  return <div></div>;
+  const { ChessData, setChessBoard } = ChessCreator();
+  const { handleDragStart, handleDragOver, handleDrop } = DragAndDrop(
+    ChessData,
+    setChessBoard
+  );
+
+  return (
+    <BoardStyle>
+      {ChessData.map((el, i) => {
+        const Check = el.ChessImg === "Empty";
+        return (
+          <Div
+            key={i}
+            id={el.id}
+            onDragOver={(e) => handleDragOver(e)}
+            onDragStart={(e) => (Check ? null : handleDragStart(e, el))}
+            onDrop={(e) => handleDrop(e)}
+            Design={el.background}
+            IMG={el.ChessImg}
+            draggable={el.ChessImg === "Empty" ? false : true}
+          ></Div>
+        );
+      })}
+    </BoardStyle>
+  );
 };
 
 export default ChessMap;
