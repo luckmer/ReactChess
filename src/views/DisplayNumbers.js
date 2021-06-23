d02cfb84b159ab01a2abe2accc5fcf0a96c13b93
luckmer <woszczo04@gmail.com>
2021-06-23 15:03:59 +0200

    numbers creator

--- src/views/DisplayNumbers.js
+++ src/views/DisplayNumbers.js
@@ -1,5 +1,14 @@
+import { Numbers } from "../css/Chess.Style";
+import { numbers } from "../hooks/Constants";
+
 const DisplayNumbers = () => {
-  return <div></div>;
+  return (
+    <Numbers>
+      {numbers.map((number) => (
+        <p key={number}>{number}</p>
+      ))}
+    </Numbers>
+  );
 };
 
 export default DisplayNumbers;
