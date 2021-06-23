0fd10cd958d9840f895001824b6b4e314b02e6ef
luckmer <woszczo04@gmail.com>
2021-06-23 14:55:28 +0200

    board panel

---
+++ src/hooks/Board/Board.js
@@ -0,0 +1,79 @@
+import BlackBishop from "../../icons/BlackBishop.png";
+import BlackKnight from "../../icons/BlackKnight.png";
+import BlackRook from "../../icons/BlackRook.png";
+import BlackQueen from "../../icons/BlackQueen.png";
+import BlackKing from "../../icons/BlackKing.png";
+import BlackPawn from "../../icons/BlackPawn.png";
+import WhitePawn from "../../icons/WhitePawn.png";
+import WhiteBishop from "../../icons/WhiteBishop.png";
+import WhiteKnight from "../../icons/WhiteKnight.png";
+import WhiteRook from "../../icons/WhiteRook.png";
+import WhiteQueen from "../../icons/WhiteQueen.png";
+import WhiteKing from "../../icons/WhiteKing.png";
+
+export const Board = [
+  [BlackRook, "black, BlackRook"],
+  [BlackKnight, "black, BlackKnight"],
+  [BlackBishop, "black, BlackBishop"],
+  [BlackQueen, "black, BlackQueen"],
+  [BlackKing, "black, BlackKing"],
+  [BlackBishop, "black, BlackBishop"],
+  [BlackKnight, "black, BlackKnight"],
+  [BlackRook, "black, BlackRook"],
+  [BlackPawn, "black, BlackPawn", true],
+  [BlackPawn, "black, BlackPawn", true],
+  [BlackPawn, "black, BlackPawn", true],
+  [BlackPawn, "black, BlackPawn", true],
+  [BlackPawn, "black, BlackPawn", true],
+  [BlackPawn, "black, BlackPawn", true],
+  [BlackPawn, "black, BlackPawn", true],
+  [BlackPawn, "black, BlackPawn", true],
+  ["Empty", "", false],
+  ["Empty", "", false],
+  ["Empty", "", false],
+  ["Empty", "", false],
+  ["Empty", "", false],
+  ["Empty", "", false],
+  ["Empty", "", false],
+  ["Empty", "", false],
+  ["Empty", "", false],
+  ["Empty", "", false],
+  ["Empty", "", false],
+  ["Empty", "", false],
+  ["Empty", "", false],
+  ["Empty", "", false],
+  ["Empty", "", false],
+  ["Empty", "", false],
+  ["Empty", "", false],
+  ["Empty", "", false],
+  ["Empty", "", false],
+  ["Empty", "", false],
+  ["Empty", "", false],
+  ["Empty", "", false],
+  ["Empty", "", false],
+  ["Empty", "", false],
+  ["Empty", "", false],
+  ["Empty", "", false],
+  ["Empty", "", false],
+  ["Empty", "", false],
+  ["Empty", "", false],
+  ["Empty", "", false],
+  ["Empty", "", false],
+  ["Empty", "", false],
+  [WhitePawn, "white, whitePawn", true],
+  [WhitePawn, "white, whitePawn", true],
+  [WhitePawn, "white, whitePawn", true],
+  [WhitePawn, "white, whitePawn", true],
+  [WhitePawn, "white, whitePawn", true],
+  [WhitePawn, "white, whitePawn", true],
+  [WhitePawn, "white, whitePawn", true],
+  [WhitePawn, "white, whitePawn", true],
+  [WhiteRook, "white, WhiteRook"],
+  [WhiteKnight, "white, WhiteKnight"],
+  [WhiteBishop, "white, WhiteBishop"],
+  [WhiteQueen, "white, WhiteQueen"],
+  [WhiteKing, "white, WhiteKing"],
+  [WhiteBishop, "white, WhiteBishop"],
+  [WhiteKnight, "white, WhiteKnight"],
+  [WhiteRook, "white, WhiteRook"],
+];
