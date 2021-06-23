60e109cb62ee2ad8a2df6febbe0854c13fce349d
luckmer <woszczo04@gmail.com>
2021-06-23 15:24:29 +0200

    king main

--- src/hooks/King/MoveKing.js
+++ src/hooks/King/MoveKing.js
@@ -1,3 +1,48 @@
-const MoveKing = () => {};
+import { KingMove } from "./PossibleMoves";
+import { TOTAL_SIZE } from "../Constants";
+import { updatePosition } from "..//DragAndDrop/updatePosition";
+import { WallCreator, KingAttack, FilterType } from "../Constants";
+
+const MoveKing = ({
+  FindTarget,
+  FindTargetType,
+  ChessData,
+  dropID,
+  setChessBoard,
+  setCurrentPlayer,
+  currentPlayer,
+}) => {
+  const king = FindTarget;
+  const KingLock = KingMove(king.id, FindTargetType);
+
+  const Wall1 = WallCreator(ChessData, 1);
+  const Wall2 = WallCreator(ChessData, 0);
+
+  const WallClear = KingLock.filter((val) => val <= TOTAL_SIZE);
+
+  if (
+    (dropID % 8 === 1 && Wall2.includes(king.id)) ||
+    (dropID % 8 === 0 && Wall1.includes(king.id))
+  ) {
+    dropID = king.id;
+  }
+
+  const existsInBothArrays = ChessData.filter((element1) =>
+    WallClear.map((element2) => element2).includes(element1.id)
+  );
+
+  const AttackPosFix = KingAttack(existsInBothArrays, dropID);
+  const SetMove = FilterType(existsInBothArrays, dropID);
+
+  const PossibleMove = [...new Set([...WallClear, ...AttackPosFix])];
+
+  if (SetMove || PossibleMove.includes(Number(dropID))) {
+    const Update = updatePosition(ChessData, dropID, FindTarget);
+    setChessBoard(Update);
+    setCurrentPlayer(currentPlayer === "white" ? "black" : "white");
+  }
+
+  return dropID;
+};
 
 export default MoveKing;
