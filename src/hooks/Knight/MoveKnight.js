3a90f26e522890ccee53c5e13d077b56c73d5e01
luckmer <woszczo04@gmail.com>
2021-06-25 12:13:26 +0200

    king fix

--- src/hooks/Knight/MoveKnight.js
+++ src/hooks/Knight/MoveKnight.js
@@ -1,7 +1,5 @@
-import { KnightMove } from "../Knight/PossibleMoves";
-import { TOTAL_SIZE, GRID_SIZE } from "../Constants";
 import { updatePosition } from "../DragAndDrop/updatePosition";
-import { WallCreator } from "../Constants";
+import KnightHelpers from "./KnightHelpers";
 
 const MoveKnight = ({
   FindTarget,
@@ -14,28 +12,7 @@ const MoveKnight = ({
 }) => {
   const knight = FindTarget;
 
-  const Wall1 = WallCreator(ChessData, 1);
-  const Wall2 = WallCreator(ChessData, 0);
-
-  const KnightLock = KnightMove(knight.id, FindTargetType).filter(
-    (el) => el <= TOTAL_SIZE && el >= 0
-  );
-
-  const ClearDataForWalls = KnightLock.filter((val) =>
-    dropID % 8 === 7 || dropID % 8 === 0
-      ? !Wall1.includes(val)
-      : dropID % 8 === 1 || dropID % 8 === 2
-      ? !Wall2.includes(val)
-      : val
-  );
-
-  const JumpFix = ClearDataForWalls.filter((val) =>
-    dropID % GRID_SIZE === 7 || dropID % GRID_SIZE === 0
-      ? val !== 48
-      : dropID % 8 === 1 || dropID % 8 === 2
-      ? val !== 17
-      : val
-  );
+  const JumpFix = KnightHelpers(ChessData, knight, FindTargetType, dropID);
 
   if (JumpFix.includes(Number(dropID))) {
     const Update = updatePosition(ChessData, dropID, FindTarget);
