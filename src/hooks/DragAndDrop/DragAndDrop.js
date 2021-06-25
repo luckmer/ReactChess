27acf9e25db64caaa43e60e7ce6257367962c469
luckmer <woszczo04@gmail.com>
2021-06-25 12:14:20 +0200

    drag and drop fix

--- src/hooks/DragAndDrop/DragAndDrop.js
+++ src/hooks/DragAndDrop/DragAndDrop.js
@@ -1,81 +1,48 @@
-import { useState } from "react";
-import { FindTheSameType, TargetType } from "../Constants";
-import MoveKnight from "../Knight/MoveKnight";
-import MoveBishop from "../Bishop/MoveBishop";
-import MovePawn from "../Pawns/MovePawn";
-import MoveKing from "../King/MoveKing";
-import MoveRook from "../Rook/MoveRook";
-import MoveQueen from "../queen/MoveQueen";
+import { useState, useContext } from "react";
+import MoveGenerator from "../Moves/MoveGenerator";
+import { DataContext } from "../../utils/Store";
+import DetectPawn from "./DetectPawn";
+import GlobalData from "./GlobalData";
 
 const DragAndDrop = (ChessData, setChessBoard) => {
   const [currentPlayer, setCurrentPlayer] = useState("white");
+  const { MOVES } = useContext(DataContext);
+  const { setMoves } = MOVES;
 
   const handleDragStart = (e, obj) => {
     const targetId = e.target.id;
     e.dataTransfer.setData("id", targetId);
+
+    const { PROPS } = GlobalData(
+      targetId,
+      ChessData,
+      targetId,
+      currentPlayer,
+      setChessBoard,
+      setCurrentPlayer,
+      setMoves
+    );
+
+    MoveGenerator(PROPS);
   };
 
-  const handleDragOver = (e) => {
-    e.preventDefault();
-  };
+  const handleDragOver = (e) => e.preventDefault();
 
   const handleDrop = (e) => {
     const Target = e.dataTransfer.getData("id");
     let dropID = e.target.id;
 
-    const FindTarget = FindTheSameType(Target, ChessData);
-    const FindObj = FindTheSameType(dropID, ChessData);
-
-    const FindTargetType = TargetType(FindTarget && FindTarget.Type, 0);
-    const FindTargetName = TargetType(FindTarget && FindTarget.Type, 1);
-    const FindObjType = TargetType(FindObj.Type, 0);
-
-    const CheckPlayer = FindTargetType === currentPlayer;
-    const DifferentColor = FindObjType !== FindTargetType;
-
-    const MOVE = FindTarget && CheckPlayer && DifferentColor;
-
-    const PROPS = {
-      FindTarget,
-      FindTargetType,
+    const { FindTargetName, MOVE, PROPS } = GlobalData(
+      Target,
       ChessData,
       dropID,
+      currentPlayer,
       setChessBoard,
       setCurrentPlayer,
-      currentPlayer,
-    };
-    //update use switch
-    if (
-      (FindTargetName === "whitePawn" && MOVE) ||
-      (FindTargetName === "BlackPawn" && MOVE)
-    ) {
-      MovePawn({ ...PROPS });
-    } else if (
-      (FindTargetName === "WhiteKing" && MOVE) ||
-      (FindTargetName === "BlackKing" && MOVE)
-    ) {
-      dropID = MoveKing({ ...PROPS });
-    } else if (
-      (FindTargetName === "WhiteKnight" && MOVE) ||
-      (FindTargetName === "BlackKnight" && MOVE)
-    ) {
-      MoveKnight({ ...PROPS });
-    } else if (
-      (FindTargetName === "WhiteBishop" && MOVE) ||
-      (FindTargetName === "BlackBishop" && MOVE)
-    ) {
-      MoveBishop({ ...PROPS });
-    } else if (
-      (FindTargetName === "WhiteRook" && MOVE) ||
-      (FindTargetName === "BlackRook" && MOVE)
-    ) {
-      MoveRook({ ...PROPS });
-    } else if (
-      (FindTargetName === "WhiteQueen" && MOVE) ||
-      (FindTargetName === "BlackQueen" && MOVE)
-    ) {
-      dropID = MoveQueen({ ...PROPS });
-    }
+      setMoves
+    );
+    dropID = DetectPawn(FindTargetName, MOVE, PROPS, dropID);
+    setMoves([]);
   };
 
   return { handleDragStart, handleDragOver, handleDrop };
